d0e3a847f9f74256a9157c2475dc15f3360c3134
moreprachid <moreprachid@gmail.com>
2025-06-08 22:00:46 +0530

    Clean the code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route, Link  } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
@@ -10,29 +11,41 @@ import Footer from "./Pages/Footer";
 
 import "./App.css";
 
+// export default function App() {
+//   return <h1>Hello, Portfolio!||||</h1>;
+// }
 
 function App() {
   return (
-    <div>
-      <nav style={{ padding: "20px", background: "rgba(13, 5, 26, 0.84)" }}>
-        <Link to="/" style={{ margin: "20px" }}>Home</Link>
-        <Link to="/about" style={{ margin: "20px" }}>About</Link>
-        <Link to="/projects" style={{ margin: "20px" }}>Projects</Link>
-        <Link to="/contact" style={{ margin: "20px" }}>Contact</Link>
-        <Link to="/Resume" style={{ margin: "20px" }}>Resume</Link>
-      </nav>
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/Resume" element={<Resume />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      
-      <Footer />
-    </div>
+        <div>
+          <nav style={{ padding: "20px", background: "rgba(13, 5, 26, 0.84)" }}>
+            <Link to="/" style={{ margin: "20px" }}>
+              Home
+            </Link>
+            <Link to="/about" style={{ margin: "20px" }}>
+              About
+            </Link>
+            <Link to="/projects" style={{ margin: "20px" }}>
+              Projects
+            </Link>
+            <Link to="/contact" style={{ margin: "20px" }}>
+              Contact
+            </Link>
+            <Link to="/Resume" style={{ margin: "20px" }}>
+              Resume
+            </Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} className="App">
+            </Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/Resume" element={<Resume />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
   );
 }
 
