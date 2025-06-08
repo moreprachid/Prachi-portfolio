b619867e0239be71fbb42c4bec9951bbded573a1
moreprachid <moreprachid@gmail.com>
2025-06-08 21:02:28 +0530

    Setup for GitHub Pages deploy

--- src/App.js
+++ src/App.js
@@ -11,45 +11,49 @@ import Footer from "./Pages/Footer";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <Router basename="/Prachi-portfolio">
-        <div>
-          <nav style={{ padding: "20px", background: "rgba(13, 5, 26, 0.84)" }}>
-            <Link to="/" style={{ margin: "20px" }}>
-              Home
-            </Link>
-            <Link to="/about" style={{ margin: "20px" }}>
-              About
-            </Link>
-            <Link to="/projects" style={{ margin: "20px" }}>
-              Projects
-            </Link>
-            <Link to="/contact" style={{ margin: "20px" }}>
-              Contact
-            </Link>
-            <Link to="/Resume" style={{ margin: "20px" }}>
-              Resume
-            </Link>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} className="App">
-            </Route>
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/Resume" element={<Resume />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    </div>
-  );
+export default function App() {
+  return <h1>Hello, Portfolio!</h1>;
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <Router basename="/Prachi-portfolio">
+//         <div>
+//           <nav style={{ padding: "20px", background: "rgba(13, 5, 26, 0.84)" }}>
+//             <Link to="/" style={{ margin: "20px" }}>
+//               Home
+//             </Link>
+//             <Link to="/about" style={{ margin: "20px" }}>
+//               About
+//             </Link>
+//             <Link to="/projects" style={{ margin: "20px" }}>
+//               Projects
+//             </Link>
+//             <Link to="/contact" style={{ margin: "20px" }}>
+//               Contact
+//             </Link>
+//             <Link to="/Resume" style={{ margin: "20px" }}>
+//               Resume
+//             </Link>
+//           </nav>
+//           <Routes>
+//             <Route path="/" element={<Home />} className="App">
+//             </Route>
+//             <Route path="/about" element={<About />} />
+//             <Route path="/projects" element={<Projects />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/Resume" element={<Resume />} />
+//             <Route path="*" element={<NotFound />} />
+//           </Routes>
+//           <Footer />
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // function App() {
 //   return (
