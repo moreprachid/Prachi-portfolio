4400b09ab2771cf1b684141cb4a0b08771edcd34
moreprachid <moreprachid@gmail.com>
2025-06-08 20:38:05 +0530

    Fix: Removed duplicate App file and updated routing

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-   <HashRouter basename="/Prachi-portfolio">
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
+  //  <HashRouter basename="/Prachi-portfolio">
+  //   <App />
+  // </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
